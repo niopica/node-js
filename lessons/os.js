a1dbe1ee12a5b0b09d75e9099c77130f4f596799
Nikita Galanzovskiy <niopica@mail.ru>
2025-01-27 18:27:34 +0500

    learn cluster

--- lessons/os.js
+++ lessons/os.js
@@ -1,6 +1,33 @@
-const os = require('os');
-
-console.log(os.platform());
-console.log(os.arch());
-console.log(os.cpus());
-console.log(os.cpus().length)
+// const os = require('os');
+// const cluster = require('cluster')
+//
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.cpus().length)
+//
+// if (cluster.isPrimary) {
+//     for (let i = 0; i < os.cpus().length - 2; i++) {
+//         cluster.fork()
+//     }
+//
+//     cluster.on('exit', (worker,) => {
+//         console.log(`Воркер с pid ${worker.process.pid} умер`);
+//         cluster.fork()
+//     });
+// } else {
+//     console.log(`Воркер с pid= ${process.pid} запущен`)
+//
+//     setInterval(() => {
+//         console.log(`Воркер с pid= ${process.pid} ещё работает`)
+//     }, 5000)
+// }
+//
+// const cpus = os.cpus()
+//
+// for (let i = 0; i < cpus.length - 2; i++) {
+//     const CPUcore = cpus[i]
+//     console.log('Запустить ещё один процесс')
+// }
+//
+// console.log(process.pid)
